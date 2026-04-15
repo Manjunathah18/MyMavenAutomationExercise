0e259ef4f35391b24bf407d0607195ca975e7998
Manjunatha H <manju.h5355@gmail.com>
2026-04-15 22:43:17 +0530

    Automation file added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,24 +1,52 @@
 package com.example;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://automationexercise.com/");
-        Thread.sleep(3000);
-        driver.findElement(By.linkText("Products")).click();
-        Thread.sleep(3000);
-        WebElement searchBox = driver.findElement(By.id("search_product"));
-        searchBox.sendKeys("Tshirt");
-        driver.findElement(By.id("submit_search")).click();
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("(//a[contains(text(),'Add to cart')])[1]")).click();
-        Thread.sleep(5000);
-        System.out.println("Product Added to Cart Successfully");
-        driver.quit();
+    public static void main(String[] args) {
+
+        // Configure Chrome for Jenkins/Linux (Headless Mode)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // REQUIRED
+        options.addArguments("--no-sandbox"); // REQUIRED
+        options.addArguments("--disable-dev-shm-usage"); // REQUIRED
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            // Open website
+            driver.get("https://automationexercise.com/");
+            Thread.sleep(3000);
+
+            // Click on Products
+            driver.findElement(By.linkText("Products")).click();
+            Thread.sleep(3000);
+
+            // Search for product
+            WebElement searchBox = driver.findElement(By.id("search_product"));
+            searchBox.sendKeys("Tshirt");
+
+            driver.findElement(By.id("submit_search")).click();
+            Thread.sleep(3000);
+
+            // Add first product to cart
+            driver.findElement(By.xpath("(//a[contains(text(),'Add to cart')])[1]")).click();
+            Thread.sleep(5000);
+
+            // Print confirmation
+            System.out.println("Product Added to Cart Successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close browser
+            driver.quit();
+        }
     }
 }
